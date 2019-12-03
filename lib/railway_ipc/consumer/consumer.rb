c8b573a619117a1a72729b3b43084794e82b813c
Steven Nuñez <steven@flatironschool.com>
2019-12-03 11:35:26 -0500

    Remove mixin for consumer message handling

--- lib/railway_ipc/consumer/consumer.rb
+++ lib/railway_ipc/consumer/consumer.rb
@@ -1,11 +1,10 @@
 require 'json'
 require 'base64'
-require 'railway_ipc/consumer/consumer_message_handling'
 
 module RailwayIpc
   class Consumer
     include Sneakers::Worker
-    include RailwayIpc::ConsumerMessageHandling
+    attr_reader :message, :handler
 
     def self.listen_to(queue:, exchange:)
       from_queue queue,
@@ -15,9 +14,30 @@ module RailwayIpc
                  connection: RailwayIpc.bunny_connection
     end
 
+    def self.handle(message_type, with:)
+      ConsumerResponseHandlers.instance.register(message: message_type, handler: with)
+    end
+
     def work(payload)
-      super
+      decoded_payload = RailwayIpc::Rabbitmq::Payload.decode(payload)
+      case decoded_payload.type
+      when *ConsumerResponseHandlers.instance.registered
+        @handler = ConsumerResponseHandlers.instance.get(decoded_payload.type).handler.new
+        message_klass = ConsumerResponseHandlers.instance.get(decoded_payload.type).message
+      else
+        @handler = RailwayIpc::NullHandler.new
+        message_klass = RailwayIpc::NullMessage
+      end
+      message = message_klass.decode(decoded_payload.message)
       handler.handle(message)
+    rescue StandardError => e
+      RailwayIpc.logger.log_exception(
+          feature: 'railway_consumer',
+          error: e.class,
+          error_message: e.message,
+          payload: payload
+      )
+      raise e
     end
   end
 end
