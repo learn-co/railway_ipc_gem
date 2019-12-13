6ae40d8e5b6b0945e6750149c4eaa0b1f16a9fc6
Lukeghenco <lukeghenco@gmail.com>
2019-12-13 14:55:59 -0800

    remove migration path setup in Rails env

--- spec/support/rails_app/config/environments/test.rb
+++ spec/support/rails_app/config/environments/test.rb
@@ -7,9 +7,6 @@ Rails.application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
-  # Use migration files in RailwayIpc gem
-  config.paths['db/migrate'] << File.join(root.parent.parent.parent, '/priv/migrations')
-
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
