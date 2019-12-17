bc076323fa1b7f81ae06382aa7c948a051f560fd
Steven Nunez <steven.nunez@gmail.com>
2019-12-17 16:02:06 -0500

    Persist messages

--- lib/railway_ipc/models/published_message.rb
+++ lib/railway_ipc/models/published_message.rb
@@ -3,6 +3,19 @@ module RailwayIpc
     self.table_name = 'railway_ipc_published_messages'
     self.primary_key = "uuid"
 
+    def self.store_message(exchange_name, message)
+      encoded_message = RailwayIpc::Rabbitmq::Payload.encode(message)
+      self.create(
+          uuid: message.uuid,
+          message_type: message.class.to_s,
+          user_uuid: message.user_uuid,
+          correlation_id: message.correlation_id,
+          encoded_message: encoded_message,
+          status: "sent",
+          exchange: exchange_name
+      )
+    end
+
     private
 
     def timestamp_attributes_for_create
