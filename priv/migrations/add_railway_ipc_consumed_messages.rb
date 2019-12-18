5684ff85ed765e69beb8858da5cfbbf1388a3e74
Lukeghenco <lukeghenco@gmail.com>
2019-12-18 07:58:06 -0800

    Add migration file for "railway_ipc_consumed_messages" table

---
+++ priv/migrations/add_railway_ipc_consumed_messages.rb
@@ -0,0 +1,19 @@
+class AddIpcMessages < ActiveRecord::Migration
+  def change
+    create_table :railway_ipc_consumed_messages, id: false do | t |
+      t.uuid :uuid, null: false
+      t.string :message_type, null: false
+      t.uuid :user_uuid
+      t.uuid :correlation_id
+      t.text :encoded_message, null: false
+      t.string :status, null: false
+      t.string :queue
+      t.string :exchange
+
+      t.datetime :updated_at
+      t.datetime :inserted_at
+    end
+
+    add_index :railway_ipc_consumed_messages, :uuid, unique: true
+  end
+end
