523940448b04882eee9c1bbd0a94a240139c234c
Lukeghenco <lukeghenco@gmail.com>
2020-01-10 07:56:35 -0800

    Destroy TestMessagesFactory

--- spec/factories/test_messages_factory.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :test_message, class: LearnIpc::Commands::TestMessage do
-    uuid { SecureRandom.uuid }
-    correlation_id { SecureRandom.uuid }
-    user_uuid { SecureRandom.uuid }
-    data { LearnIpc::Commands::TestMessage::Data.new(iteration: "bk-001") }
-  end
-end
