33b4f074b79c2d42563f05dda90e61d9d37e637f
Bob Nadler <bnadlerjr@gmail.com>
2020-07-07 17:37:54 -0400

    Extract logic from consumer, placing it into a process class

---
+++ lib/railway_ipc/consumer/process_incoming_message.rb
@@ -0,0 +1,103 @@
+# frozen_string_literal: true
+
+module RailwayIpc
+  class ProcessIncomingMessage
+    class UnknownMessageJob
+      attr_reader :incoming_message, :logger
+
+      def initialize(incoming_message, logger)
+        @incoming_message = incoming_message
+        @logger = logger
+      end
+
+      def status
+        'unknown_message_type'
+      end
+
+      def run
+        logger.warn(
+          incoming_message.decoded,
+          "Ignoring unknown message of type '#{incoming_message.type}'"
+        )
+      end
+    end
+
+    class IgnoredMessageJob
+      attr_reader :incoming_message, :logger
+
+      def initialize(incoming_message, logger)
+        @incoming_message = incoming_message
+        @logger = logger
+      end
+
+      def status
+        'ignored'
+      end
+
+      def run
+        logger.warn(
+          incoming_message.decoded,
+          "Ignoring message, no registered handler for '#{incoming_message.type}'"
+        )
+      end
+    end
+
+    class NormalMessageJob
+      attr_reader :incoming_message, :handler, :status
+
+      def initialize(incoming_message, handler)
+        @incoming_message = incoming_message
+        @handler = handler
+        @status = 'not_processed'
+      end
+
+      def run
+        result = handler.handle(incoming_message.decoded)
+        @status = result.success? ? 'success' : 'failed_to_process'
+      end
+    end
+
+    attr_reader :consumer, :incoming_message, :logger
+
+    def self.call(consumer, incoming_message)
+      new(consumer, incoming_message).call
+    end
+
+    def initialize(consumer, incoming_message, logger: RailwayIpc.logger)
+      @consumer = consumer
+      @incoming_message = incoming_message
+      @logger = logger
+    end
+
+    def call
+      raise_message_invalid_error unless incoming_message.valid?
+      message = find_or_create_consumed_message
+      return if message.processed?
+
+      message.update_with_lock(classify_message)
+    end
+
+    private
+
+    def raise_message_invalid_error
+      error =  "Message is invalid: #{incoming_message.stringify_errors}."
+      logger.error(incoming_message.decoded, error)
+      raise RailwayIpc::IncomingMessage::InvalidMessage.new(error)
+    end
+
+    def find_or_create_consumed_message
+      RailwayIpc::ConsumedMessage.find_by(uuid: incoming_message.uuid) ||
+        RailwayIpc::ConsumedMessage.create_processing(consumer, incoming_message)
+    end
+
+    def classify_message
+      if incoming_message.decoded.is_a?(RailwayIpc::Messages::Unknown)
+        UnknownMessageJob.new(incoming_message, logger)
+      elsif handler = consumer.get_handler(incoming_message.type)
+        NormalMessageJob.new(incoming_message, handler)
+      else
+        IgnoredMessageJob.new(incoming_message, logger)
+      end
+    end
+  end
+end
---
+++ spec/railway_ipc/consumer/process_incoming_message_spec.rb
@@ -0,0 +1,187 @@
+# frozen_string_literal: true
+
+RSpec.describe RailwayIpc::ProcessIncomingMessage, '#call' do
+  let(:fake_logger) { RailwayIpc::SpecHelpers::FakeLogger.new }
+  let(:fake_handler) { RailwayIpc::SpecHelpers::FakeHandler.new }
+
+  around(:each) do |example|
+    original_logger = RailwayIpc.logger.logger
+    RailwayIpc.configure(logger: fake_logger)
+    example.run
+    RailwayIpc.configure(logger: original_logger)
+  end
+
+  context 'when the message is valid' do
+    let(:protobuf) { stubbed_protobuf }
+    let(:payload) { stubbed_payload }
+    let(:incoming_message) { RailwayIpc::IncomingMessage.new(payload) }
+    let(:fake_handler) { RailwayIpc::SpecHelpers::FakeHandler.new }
+
+    context 'and the consumer provides a handler' do
+      it 'executes the message handler using the message' do
+        consumer = instance_double(
+          RailwayIpc::Consumer,
+          queue_name: 'my-queue',
+          exchange_name: 'my-exchange',
+          get_handler: fake_handler
+        )
+
+        process = described_class.new(consumer, incoming_message)
+        process.call
+        expect(fake_handler.called?).to eq(true)
+        expect(fake_handler.handled_message).to eq(protobuf)
+      end
+
+      it "creates a new consumed message with the handler's response" do
+        consumer = instance_double(
+          RailwayIpc::Consumer,
+          queue_name: 'my-queue',
+          exchange_name: 'my-exchange',
+          get_handler: fake_handler
+        )
+
+        process = described_class.new(consumer, incoming_message)
+
+        expect {
+          process.call
+        }.to change {
+          RailwayIpc::ConsumedMessage.count
+        }.by(1)
+
+        consumed_message = RailwayIpc::ConsumedMessage.last
+        expect(consumed_message.uuid).to eq(RailwayIpc::SpecHelpers::DEAD_BEEF_UUID)
+        expect(consumed_message.status).to eq('success')
+        expect(consumed_message.message_type).to eq('RailwayIpc::Messages::TestMessage')
+        expect(consumed_message.user_uuid).to eq(RailwayIpc::SpecHelpers::BAAD_FOOD_UUID)
+        expect(consumed_message.correlation_id).to eq(RailwayIpc::SpecHelpers::CAFE_FOOD_UUID)
+        expect(consumed_message.queue).to eq('my-queue')
+        expect(consumed_message.exchange).to eq('my-exchange')
+        expect(consumed_message.encoded_message).to eq(payload)
+      end
+
+      it 'sets the correct consumed message status if the handler fails' do
+        fake_handler.result = false
+        consumer = instance_double(
+          RailwayIpc::Consumer,
+          queue_name: 'my-queue',
+          exchange_name: 'my-exchange',
+          get_handler: fake_handler
+        )
+
+        process = described_class.new(consumer, incoming_message)
+        process.call
+        consumed_message = RailwayIpc::ConsumedMessage.last
+        expect(consumed_message.status).to eq('failed_to_process')
+      end
+    end
+
+    context 'and the message has already been processed' do
+      it 'does not call the handler, and does not store a new message' do
+        consumer = instance_double(
+          RailwayIpc::Consumer,
+          queue_name: 'my-queue',
+          exchange_name: 'my-exchange',
+          get_handler: fake_handler
+        )
+        RailwayIpc::ConsumedMessage.create!(
+          uuid: incoming_message.uuid,
+          status: 'success',
+          message_type: incoming_message.type,
+          user_uuid: incoming_message.user_uuid,
+          correlation_id: incoming_message.correlation_id,
+          queue: consumer.queue_name,
+          exchange: consumer.exchange_name,
+          encoded_message: incoming_message.payload
+        )
+
+        process = described_class.new(consumer, incoming_message)
+        expect {
+          process.call
+        }.not_to change{RailwayIpc::ConsumedMessage.count}
+        expect(fake_handler.called?).to eq(false)
+      end
+    end
+
+    context 'and the consumer does not provide a handler' do
+      it 'saves the consumed message with a status of `ignored` and logs that it cannot be handled' do
+        consumer = instance_double(
+          RailwayIpc::Consumer,
+          queue_name: 'my-queue',
+          exchange_name: 'my-exchange',
+          get_handler: nil
+        )
+
+        process = described_class.new(consumer, incoming_message)
+        process.call
+        consumed_message = RailwayIpc::ConsumedMessage.find_by(uuid: incoming_message.uuid)
+        expect(consumed_message.status).to eq('ignored')
+        expect(fake_logger.messages[:warn].first).to \
+          include(%q{Ignoring message, no registered handler for 'RailwayIpc::Messages::TestMessage'})
+      end
+    end
+  end
+
+  context 'when the message is for a unknown protobuf' do
+    it 'saves the consumed message with a status of `unknown`' do
+      consumer = instance_double(
+        RailwayIpc::Consumer ,
+        queue_name: 'my-queue',
+        exchange_name: 'my-exchange',
+        get_handler: fake_handler
+      )
+
+      payload = {
+        type: 'Foo',
+        encoded_message: Base64.encode64(RailwayIpc::Messages::TestMessage.encode(stubbed_protobuf))
+      }.to_json
+
+      incoming_message = RailwayIpc::IncomingMessage.new(payload)
+      process = described_class.new(consumer, incoming_message)
+      process.call
+      consumed_message = RailwayIpc::ConsumedMessage.last
+      expect(consumed_message.status).to eq('unknown_message_type')
+      expect(consumed_message.encoded_message).to eq(payload)
+    end
+
+    it 'logs an warning' do
+      consumer = instance_double(
+        RailwayIpc::Consumer ,
+        queue_name: 'my-queue',
+        exchange_name: 'my-exchange',
+        get_handler: fake_handler
+      )
+
+      payload = {
+        type: 'Foo',
+        encoded_message: Base64.encode64(RailwayIpc::Messages::TestMessage.encode(stubbed_protobuf))
+      }.to_json
+
+      incoming_message = RailwayIpc::IncomingMessage.new(payload)
+      process = described_class.new(consumer, incoming_message)
+      process.call
+      expect(fake_logger.messages[:warn].first).to \
+        include("Ignoring unknown message of type 'Foo'")
+    end
+  end
+
+  context 'when the incoming message is invalid (ie. missing correlation ID)' do
+    it 'raises an error, logs a message, and does not store message' do
+      consumer = instance_double(RailwayIpc::Consumer)
+
+      payload = {
+        type: 'RailwayIpc::Messages::TestMessage',
+        encoded_message: Base64.encode64(RailwayIpc::Messages::TestMessage.encode(stubbed_protobuf(correlation_id: "")))
+      }.to_json
+
+      incoming_message = RailwayIpc::IncomingMessage.new(payload)
+      process = described_class.new(consumer, incoming_message)
+      expect {
+        process.call
+      }.to(raise_error(RailwayIpc::IncomingMessage::InvalidMessage))
+      expect(fake_logger.messages[:error].first).to \
+        include('Message is invalid: correlation_id is required.')
+
+      expect(RailwayIpc::ConsumedMessage.count).to be_zero
+    end
+  end
+end
