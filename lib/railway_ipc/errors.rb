cd1ef27bf6a50a70383628fe99aa533d8505721e
Bob Nadler <bnadlerjr@gmail.com>
2020-07-08 19:55:28 -0400

    Rubocop: Disable Style/ClassAndModuleChildren for error classes

--- lib/railway_ipc/errors.rb
+++ lib/railway_ipc/errors.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+# rubocop:disable Style/ClassAndModuleChildren
 class RailwayIpc::Error < StandardError; end
 class RailwayIpc::InvalidProtobuf < RailwayIpc::Error; end
 class RailwayIpc::IncomingMessage::ParserError < RailwayIpc::Error; end
 class RailwayIpc::IncomingMessage::InvalidMessage < RailwayIpc::Error; end
+# rubocop:enable Style/ClassAndModuleChildren
